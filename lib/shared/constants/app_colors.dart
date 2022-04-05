70b642aba95bb826d03118113460e09fbfcd53c6
Kaio Costa de Oliveira <hello@kaio.dev>
2022-04-05 20:10:19 -0300

    feature: constants

---
+++ lib/shared/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const Color primary = Color(0xff1a87dd);
+  static const Color secondary = Color(0xfff8bb18);
+
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0xff1a1a1a);
+  static const Color grey = Color(0xffc4c4c4);
+
+  static const Color inputBorder = Color(0xffc6c6c6);
+}
---
+++ lib/shared/constants/app_text_style.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/widgets.dart';
+import 'package:wallet/shared/constants/app_colors.dart';
+
+class AppTextStyle {
+  static const TextStyle h1 = TextStyle(
+    fontSize: 32.0,
+    fontWeight: FontWeight.w700,
+    color: AppColors.black,
+  );
+
+  static const TextStyle button = TextStyle(
+    fontSize: 14.0,
+  );
+
+  static const TextStyle inputLabel = TextStyle(
+    color: AppColors.black,
+    fontSize: 14.0,
+  );
+}
