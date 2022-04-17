62d50d0017527e571bfbf1f0e63e50fca7a2a45e
Kaio Costa de Oliveira <hello@kaio.dev>
2022-04-17 09:52:16 -0300

    update: atualiza constantes e variáveis de ambiente

--- lib/app_controller.dart
+++ lib/app_controller.dart
@@ -1,6 +1,9 @@
+import 'package:wallet/shared/models/auth_data.dart';
+
 import 'shared/services/dio/custom_dio.dart';
 import 'shared/services/http/http_services.dart';
 
 class AppController {
   final HttpService dio = CustomDio();
+  late AuthData authData;
 }
--- lib/shared/constants/api_routes.dart
+++ lib/shared/constants/api_routes.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: unnecessary_string_interpolations
+
+import 'package:wallet/shared/models/enviroment.dart';
+
 class ApiRoutes {
-  static String baseUrl = '';
+  static String baseUrl = '''${Enviroment.urlBase}''';
 }
